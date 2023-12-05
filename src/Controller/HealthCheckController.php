894a7e8df27321c6b72e1cb0a0188e0df9b16b7a
scl <arausseo@sclconsultores.cl>
2023-12-05 20:13:16 -0300

    configuring some packages and general api routes

---
+++ src/Controller/HealthCheckController.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations\Route;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+#[Route(path: "/api/health-check", name: "api_health-check-")]
+class HealthCheckController extends AbstractFOSRestController
+{
+    #[Rest\Get('', name: 'list')]
+    public function healthCheck(): Response
+    {
+        return new JsonResponse('health check online', Response::HTTP_OK);
+    }
+}
